efe4584d165bc059a193319afb96535d9819fe50
Kuba H <kuba.hefner@gmail.comS>
2025-05-23 14:14:11 +0200

    dodanie podsatwowej struktury html

--- logowanie.php
+++ logowanie.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl-PL">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Forum o psach</title>
+    <link rel="stylesheet" href="styl4.css">
 </head>
 <body>
-    
+    <div class="baner"></div>
+    <div class="flex">
+        <div class="lewy"></div>
+        <div class="prawy"></div>
+    </div>
+    <div class="stopka"></div>
 </body>
 </html>
